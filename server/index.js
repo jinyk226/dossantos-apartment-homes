4d4cb96b01c69ce9197718d0568362d4225519a8
Jinyoung Kim <jinyk226@gmail.com>
2022-03-07 21:33:43 -0800

    basic backend set up

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Parse incoming requests with JSON payloads (Plain english: Formats incoming requests to JSON format for our code to read)
+app.use(express.json())
+
+// Allow files in public folder to be accessible by our express app
+app.use(express.static(path.join(__dirname, '../public')))
+
+// 400 Error Handler: Send index.html for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// 500 Error handler
+app.use((req, res, next, err) => {
+  res.status(err.status || 500).send(err.message || 'Internal server error')
+})
+
+module.exports = app
