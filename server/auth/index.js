9f454ae11dbd7448ccfbc6b826c7a660f764ad69
jinyk <jinyk226@gmail.com>
2022-04-20 08:22:57 -0700

    logout created and refresh error resolved

--- server/auth/index.js
+++ server/auth/index.js
@@ -11,11 +11,11 @@ router.post('/signup', async (req, res, next) => {
         const {firstName, lastName, email, password} = req.body;
 
         const found = await User.findOne({where: {email}})
-        if (found) res.status(400).send("User already exists!")
+        if (found) return res.status(400).send("User already exists!")
         else {
             const hashPass = await bcrypt.hash(password, 10);
             const user = await User.create({ email, password: hashPass, firstName, lastName})
-            res.status(201).send(user)
+            return res.status(201).send(user)
         }
     } catch (err) {
         console.log(err)
@@ -29,16 +29,16 @@ router.post('/login', async (req, res, next) => {
 
         const user = await User.findOne({where: {email}})
         if (user) {
-            if(bcrypt.compare(password, user.password)) {
+            if(await bcrypt.compare(password, user.password)) {
                 const accessToken = jwt.sign({email: user.email, password: user.password}, process.env.JWT_SECRET_KEY, {expiresIn: "30 min"})
                 const refreshToken = jwt.sign({email: user.email, password: user.password}, process.env.JWT_REFRESH_SECRET_KEY)
                 await RefreshToken.create({refreshToken})
-                res.send({accessToken, refreshToken})
+                return res.send({accessToken, refreshToken})
             } else {
-                res.status(401).send("Incorrect password, please try again")
+                return res.status(401).send("Incorrect password, please try again")
             }
         } else {
-            res.status(400).send("Unable to find this user. Click 'sign up' to create an account!")
+            return res.status(400).send("Unable to find this user. Click 'sign up' to create an account!")
         }
     } catch (err) {
         console.log(err)
@@ -50,15 +50,25 @@ router.post('/refresh', async (req, res, next) => {
     const { refreshToken } = req.body
     if (refreshToken == null) return res.sendStatus(401)
     // attempt to find refresh token in database. If not found, send status 403
-    const dbRefreshToken = RefreshToken.findOne({where: {refreshtoken: refreshToken}})
-    if (!dbRefreshToken) res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY, (err, user) => {
-        if (err) return res.sendStatus(403)
+    const dbRefreshToken = await RefreshToken.findOne({where: {refreshToken}})
+    if (!dbRefreshToken) return res.sendStatus(403)
+    await jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.sendStatus(403)
+        }
         const accessToken = jwt.sign({ email: user.email, password: user.password}, process.env.JWT_SECRET_KEY, {expiresIn: "30 min"})
-        res.send(accessToken)
+        return res.send({accessToken})
     })
+    next()
 })
 
 // /auth/logout
+router.delete('/logout', async (req, res, next) => {
+    const { refreshToken } = req.body
+    if (refreshToken == null) res.sendStatus(400)
+    const userToken = await RefreshToken.findOne({where: {refreshToken}})
+    await userToken.destroy()
+    return res.status(200).send("User is successfully logged out")
+})
 
 module.exports = router;
