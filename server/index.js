7272cea70e6b6e011723d6a2325a227fa08615ac
jinyk <jinyk226@gmail.com>
2022-04-20 08:35:02 -0700

    auth server separated from api server

--- server/index.js
+++ server/index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const authApp = express()
 
 // Parse incoming requests with JSON payloads (Plain english: Formats incoming requests to JSON format for our code to read)
 app.use(express.json())
+authApp.use(express.json())
 
 // Allow files in public folder to be accessible by our express app
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api', require('./api/'))
-
-app.use('/auth', require('./auth'))
 
 // 400 Error Handler: Send index.html for any other requests
 app.get('*', (req, res) => {
@@ -23,4 +23,18 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || 'Internal server error')
 })
 
-module.exports = app
+
+
+authApp.use('/auth', require('./auth'))
+
+// 400 Error Handler: Send index.html for any other requests
+authApp.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// 500 Error handler
+authApp.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message || 'Internal server error')
+})
+
+module.exports = {app, authApp}
