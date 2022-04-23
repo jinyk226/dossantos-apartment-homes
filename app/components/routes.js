bcad949c8a77ff09e8c51058c999ff0f0c01bd55
jinyk <jinyk226@gmail.com>
2022-04-23 15:03:13 -0700

    front end auth form created, README updated

--- app/components/routes.js
+++ app/components/routes.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
 import Home from './home'
-import NavBar from './navbar'
 import AboutUs from './aboutUs'
 import ContactUs from './contactUs'
+import Auth from './auth'
 
 const app = () => {
     return (
@@ -13,6 +13,7 @@ const app = () => {
                 <Route path='/home' element={<Home />} />
                 <Route path='/about-us' element={<AboutUs />} />
                 <Route path='/contact-us' element={<ContactUs />} />
+                <Route path='/auth' element={<Auth />} />
                 <Route path="*" element={<Navigate replace to="/home" />} />
             </Routes>
         </Router>
